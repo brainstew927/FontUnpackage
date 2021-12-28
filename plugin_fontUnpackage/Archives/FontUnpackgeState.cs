ca8645347bb1cbc0b1afee7a3f4afdf1fe58dbb4
Lorenzo <brainstew927@gmail.com>
2021-12-28 22:33:10 +0100

    initial commit

---
+++ plugin_fontUnpackage/Archives/FontUnpackgeState.cs
@@ -0,0 +1,30 @@
+﻿using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models.Archive;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace plugin_fontUnpackage.Archives
+{
+    class FontUnpackageState : IArchiveState, ILoadFiles
+    {
+        // Exposes the loaded files from the archive format to any consuming user interface
+        public IList<IArchiveFileInfo> Files { get; private set; }
+
+        public async Task Load(IFileSystem fileSystem, UPath filePath, LoadContext context)
+        {
+            var input = await fileSystem.OpenFileAsync(filePath);
+
+            // ... Load archive files from input into 'Files' property
+            // Example: Files = new List<IArchiveFileInfo>(); /* for an empty list of files
+            // ora ho in input lo stream, cerco di leggerlo
+            
+        }
+
+    }
+}
---
+++ plugin_fontUnpackage/Archives/fontUnpackagePlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+
+namespace plugin_fontUnpackage.Archives
+{
+    public class FontUnpackagePlugin : IFilePlugin
+    {
+        // TODO: generare la mia GUID
+        public Guid PluginId => Guid.Parse("bab218f4-550f-40ee-9219-d83b11265883");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.bin" };
+        public PluginMetadata Metadata { get; }
+
+        public FontUnpackagePlugin()
+        {
+            Metadata = new PluginMetadata("fontUnpackage", "brainstew927", "The main texture resource archive in Streets Of Rage 4.");
+        }
+
+        public IPluginState CreatePluginState(IBaseFileManager fileManager)
+        {
+            return new FontUnpackageState();
+        }
+    }
+}
