d1e8aaf53028416b16c300c745a834a19493b4e3
Lorenzo <brainstew927@gmail.com>
2021-12-29 01:38:33 +0100

    va per la maggior parte. problemi con alcuni file e spacer

--- plugin_fontUnpackage/Archives/FontUnpackage.cs
+++ plugin_fontUnpackage/Archives/FontUnpackage.cs
@@ -31,7 +31,7 @@ namespace plugin_fontUnpackage.Archives
             var files = new List<IArchiveFileInfo>();
             // leggo per prima cosa il file count (sono i primi 32 bit)
             // oggetto usato per leggere una certa qta di bit per 
-            var br = new BinaryReaderX(input, false);
+            var br = new BinaryReaderX(input, true);
             byte[] file_count_BYTES = new byte[4];
             br.Read(file_count_BYTES, 0, 4);
             // ottengo il valore contenuto in file_count
@@ -64,17 +64,30 @@ namespace plugin_fontUnpackage.Archives
                 stream = new SubStream(input, 4 * (i + 2), 4);
                 reader_ = new BinaryReaderX(stream, false);
                 UInt32 off_fine = reader_.ReadType<UInt32>();
-                files.Add(new ArchiveFileInfo(new MemoryStream(BitConverter.GetBytes(off_fine)), i.ToString("D8")));
+                //files.Add(new ArchiveFileInfo(new MemoryStream(BitConverter.GetBytes(off_fine)), i.ToString("D8")));
 
-                /*
-                // leggo i dati 
-                // prendo la dimensione tot in byte
-                UInt32 size = off_fine - off_partenza;
-                byte[] buffer = new byte[size];
-                br.Read(buffer, (int)off_partenza, (int)size);
-                // abbiamo dentro buffer il nostro file da decomprimere poi con lo script
-                files.Add(new ArchiveFileInfo(new MemoryStream(buffer), $"file_{i}"));
-                */
+
+                off_fine -= 1;
+                var size = off_fine - off_partenza;
+
+                if(off_fine < off_partenza || off_fine <= 1 || size <= 0)
+                {
+                    files.Add(new ArchiveFileInfo(new MemoryStream(BitConverter.GetBytes(off_fine)), i.ToString("D8") + "_PROBLEMA"));
+
+                    continue;
+                }
+
+                // creo il nuovo stream (che contiene SOLO i dati del file)
+                stream = new SubStream(input, off_partenza, size);
+                // creo un nuovo reader che legge da strewam
+                reader_ = new BinaryReaderX(stream, false);
+                // leggo i dati del file e li metto in un array di Byte
+                byte[] data = new byte[size];
+                for (var j = 0; j < off_fine - off_partenza; j++)
+                {
+                    data[j] = reader_.ReadByte();
+                }
+                files.Add(new ArchiveFileInfo(new MemoryStream(data), i.ToString("D8")));
             }
 
 
